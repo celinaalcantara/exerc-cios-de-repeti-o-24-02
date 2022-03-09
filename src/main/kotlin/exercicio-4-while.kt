0a1631066390bb6bfc1a8a6225f6458ce7599264
Celina Alcantara <celinaacarmo@gmail.com>
2022-03-09 14:12:16 -0300

    Atividade repetição - 24/02

--- src/main/kotlin/exercicio-4-while.kt
+++ src/main/kotlin/exercicio-4-while.kt
@@ -1,7 +1,4 @@
-import java.sql.DriverManager.println
-
-fun main (){
-    /* Uma empresa desenvolveu uma pesquisa para saber as características psicológicas
+/* Uma empresa desenvolveu uma pesquisa para saber as características psicológicas
      dos indivíduos de uma região. Para tanto, a cada uma das pessoas era perguntado:
      idade, sexo (1-feminino / 2-masculino / 3-Outros), e as opções (1, se a pessoa
      era calma; 2, se a pessoa era nervosa e 3, se a pessoa era agressiva). Pede-se
@@ -13,31 +10,36 @@ fun main (){
      o número de pessoas nervosas com mais de 40 anos;
      o número de pessoas calmas com menos de 18 anos.
  */
-    var idade = 0
-    var sexo = 0
-    var opcao = 0
-    var cont = 0
+var idade = 0
+var sexo = 0
+var opcao = 0
+var cont = 0
 
-    var mNerv = 0
-    var hAgr = 0
-    var oCalm = 0
-    var pnAcima40 = 0
-    var pcAbaixo18 = 0
+var pCalmas = 0
+var mNerv = 0
+var hAgr = 0
+var oCalm = 0
+var pnAcima40 = 0
+var pcAbaixo18 = 0
 
-    while (cont < 1){
+while (cont < 150){
 
-        println("Pessoa ${cont+1}")
+    println("Pessoa ${cont + 1}")
 
-        println("Digite a idade: ")
-        idade = readLine()!!.toInt()
+    println("Digite a idade: ")
+    idade = readLine()!!.toInt()
 
-        println("Digite o sexo, 1 para feminino, 2 para masculino e 3 para outros: ")
-        sexo = readLine()!!.toInt()
+    println("Digite o sexo, 1 para feminino, 2 para masculino e 3 para outros: ")
+    sexo = readLine()!!.toInt()
 
-        println("Digite seu sexo, 1 para feminino, 2 para masculino e 3 para outros: ")
-        opcao = readLine()!!.toInt()
+    println("Digite o humor, 1 para calma, 2 para nervosa e 3 para agressiva: ")
+    opcao = readLine()!!.toInt()
 
-        cont++
+    cont++
+
+
+    if(opcao ==1) {
+        pCalmas++
 
     }
     if(sexo == 1 && opcao ==2){
@@ -60,6 +62,13 @@ fun main (){
         pcAbaixo18++
     }
 
-    cont++
+
+}
+println("O número de pessoas calmas é: $pCalmas\n" +
+"O número de mulheres nervosas é: $mNerv\n" +
+"0 número de homens agressivos é: $hAgr\n" +
+"O número de outros calmos é: $oCalm\n" +
+"O número de pessoas nervosas com mais de 40 anos é: $pnAcima40\n" +
+"O número de pessoas calmas com menos de 18 anos é: $pcAbaixo18")
 
 }
